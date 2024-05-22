3c5ac8be976afbe0cc8324d1552bc5d9870b4ec3
WolfGGO <klimenkomisa1@gmail.com>
2024-05-22 09:10:48 +0300

    Добавьте файлы проекта.

---
+++ лр26/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace лр26
+{
+    public class Database
+    {
+        // Поле для хранения строки подключения к базе данных
+        private string _connectionString;
+
+        // Конструктор, принимающий строку подключения
+        public Database(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // Свойство для получения подключения к базе данных
+        public IDbConnection Connection => new SqlConnection(_connectionString);
+
+        // Метод для получения списка консультаций
+        public IEnumerable<Consultation> GetConsultations()
+        {
+            using (var db = Connection)
+            {
+                // Выполнение запроса SELECT и возврат списка консультаций
+                return db.Query<Consultation>("SELECT * FROM Consultations");
+            }
+        }
+
+        // Метод для добавления новой консультации
+        public void AddConsultation(Consultation consultation)
+        {
+            using (var db = Connection)
+            {
+                // SQL-запрос для вставки новой консультации
+                var sql = "INSERT INTO Consultations (FullName, Subject, Date, TimeSlot, IsBooked) VALUES (@FullName, @Subject, @Date, @TimeSlot, @IsBooked)";
+                db.Execute(sql, consultation); // Выполнение запроса
+            }
+        }
+
+        // Метод для обновления статуса бронирования консультации
+        public void UpdateConsultationBooking(int id, bool isBooked)
+        {
+            using (var db = Connection)
+            {
+                // SQL-запрос для обновления статуса бронирования консультации
+                var sql = "UPDATE Consultations SET IsBooked = @IsBooked WHERE Id = @Id";
+                db.Execute(sql, new { Id = id, IsBooked = isBooked }); // Выполнение запроса
+            }
+        }
+
+        // Метод для удаления консультации по ID
+        public void DeleteConsultation(int id)
+        {
+            using (var db = Connection)
+            {
+                // SQL-запрос для удаления консультации
+                var sql = "DELETE FROM Consultations WHERE Id = @Id";
+                db.Execute(sql, new { Id = id }); // Выполнение запроса
+            }
+        }
+
+        // Метод для поиска консультаций по предмету
+        public IEnumerable<Consultation> SearchConsultations(string subject)
+        {
+            using (var db = Connection)
+            {
+                // SQL-запрос для поиска консультаций по предмету
+                var sql = "SELECT * FROM Consultations WHERE Subject = @Subject";
+                return db.Query<Consultation>(sql, new { Subject = subject }); // Выполнение запроса и возврат списка консультаций
+            }
+        }
+    }
+
+    // Класс для представления консультации
+    public class Consultation
+    {
+        public int Id { get; set; } // ID консультации
+        public string FullName { get; set; } // Полное имя
+        public string Subject { get; set; } // Предмет
+        public DateTime Date { get; set; } // Дата консультации
+        public string TimeSlot { get; set; } // Время консультации
+        public bool IsBooked { get; set; } // Статус бронирования
+    }
+}
---
+++ лр26/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лр26
+{
+    public partial class Form1 : Form
+    {
+        // Поле для хранения объекта базы данных
+        private Database _database;
+
+        // Поле для хранения выбранного ID консультации
+        private int _selectedId = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Инициализация объекта базы данных с подключением к файлу базы данных
+            _database = new Database(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\работа\Практика КПиЯП\лр26\лр26\ConsultationDB.mdf;Integrated Security=True;Connect Timeout=30");
+            // Загрузка консультаций при запуске формы
+            LoadConsultations();
+        }
+
+        // Метод для загрузки консультаций из базы данных в DataGridView
+        private void LoadConsultations()
+        {
+            var consultations = _database.GetConsultations();
+            dataGridView1.DataSource = consultations.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Загрузка данных в таблицу Consultations из DataSet при загрузке формы
+            this.consultationsTableAdapter.Fill(this.consultationDBDataSet.Consultations);
+        }
+
+        // Обработчик события нажатия кнопки добавления новой консультации
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var consultation = new Consultation
+            {
+                FullName = textBox1.Text,
+                Subject = comboBox1.Text,
+                Date = dateTimePicker1.Value,
+                TimeSlot = maskedTextBox1.Text,
+                IsBooked = false
+            };
+            // Добавление новой консультации в базу данных
+            _database.AddConsultation(consultation);
+            // Перезагрузка списка консультаций
+            LoadConsultations();
+            // Очистка полей ввода
+            ClearInputFields();
+        }
+
+        // Обработчик события нажатия кнопки удаления выбранной консультации
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_selectedId > -1)
+            {
+                _database.DeleteConsultation(_selectedId);
+                LoadConsultations();
+                ClearInputFields();
+            }
+        }
+
+        // Обработчик события нажатия кнопки бронирования выбранной консультации
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_selectedId > -1)
+            {
+                var selectedConsultation = (Consultation)dataGridView1.CurrentRow.DataBoundItem;
+                if (!selectedConsultation.IsBooked)
+                {
+                    _database.UpdateConsultationBooking(_selectedId, true);
+                    LoadConsultations();
+                    ClearInputFields();
+                }
+                else
+                {
+                    MessageBox.Show("Консультация уже занята.");
+                }
+            }
+        }
+
+        // Обработчик события нажатия кнопки отмены бронирования выбранной консультации
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (_selectedId > -1)
+            {
+                var selectedConsultation = (Consultation)dataGridView1.CurrentRow.DataBoundItem;
+                if (selectedConsultation.IsBooked)
+                {
+                    _database.UpdateConsultationBooking(_selectedId, false);
+                    LoadConsultations();
+                    ClearInputFields();
+                }
+                else
+                {
+                    MessageBox.Show("Консультация уже свободна.");
+                }
+            }
+        }
+
+        // Обработчик события нажатия кнопки обновления списка консультаций
+        private void button6_Click(object sender, EventArgs e)
+        {
+            LoadConsultations();
+        }
+
+        // Обработчик события нажатия кнопки поиска консультаций по предмету
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var consultations = _database.SearchConsultations(comboBox1.Text);
+            dataGridView1.DataSource = consultations.ToList();
+        }
+
+        // Обработчик события клика по ячейке DataGridView для выбора консультации
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                _selectedId = (int)dataGridView1.Rows[e.RowIndex].Cells[0].Value;
+            }
+        }
+
+        // Метод для очистки полей ввода
+        private void ClearInputFields()
+        {
+            textBox1.Clear();
+            comboBox1.SelectedIndex = -1;
+            dateTimePicker1.Value = DateTime.Now;
+            maskedTextBox1.Clear();
+        }
+    }
+}
